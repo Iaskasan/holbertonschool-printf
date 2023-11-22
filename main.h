31b203ee347f39700e8636d17813eb484c1302aa
Iaskasan <nokiesan@yahoo.fr>
2023-11-22 08:30:19 -0800

    added format handler structure in main.h file, actualised the print_strings function

--- main.h
+++ main.h
@@ -8,5 +8,16 @@
 int _putchar(const char c);
 int _printf(const char *format, ...);
 int _strlen(char *s);
+int print_character(va_list arguments);
+int print_string(va_list arguments);
+int print_percentage(va_list arguments);
+int print_number(va_list arguments);
+
+
+typedef struct format_handler
+{
+	char format;
+	int (*f)(va_list);
+} handler;
 
 #endif
--- print_strings.c
+++ print_strings.c
@@ -1,28 +1,26 @@
 #include "main.h"
 
 /**
- * print_strings - prints strings
- * @n: the number of parameters
+ * print_strings - Prints strings followed by a newline
+ * @args: A variable argument list of strings
+ * Return: always returns 1 if success
  */
-void print_strings(const unsigned int n, ...)
+int print_strings(va_list args)
 {
-	unsigned int i;
-	va_list args;
 	char *str;
 	int strlen;
 
-	va_start(args, n);
-	for (i = 0 ; i < n ; i++)
+	while (va_arg(args, char*) != NULL)
 	{
 		str = va_arg(args, char*);
 		strlen = _strlen(str);
 		if (str == NULL)
 		{
-			write(1, "(nil)", strlen);
-			continue;
+			break;
 		}
 		write(1, str, strlen);
 	}
 	_putchar('\n');
 	va_end(args);
+	return (1);
 }
