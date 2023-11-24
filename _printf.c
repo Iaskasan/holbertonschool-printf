d278c7d4c22d457bbbbca247871f7ac428a9b167
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2023-11-24 11:50:10 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -8,41 +8,41 @@
 
 int _printf(const char *format, ...)
 {
-    int i = 0;
-    int len = 0;
-    int result;
+	int i = 0;
+	int len = 0;
+	int result;
 
-    va_list args;
+	va_list args;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-    {
-        return (-1);
-    }
-    while (format[i] != '\0')
-    {
-        if (format[i] == '%')
-        {
-            result = format_handler(format[i + 1], args);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	{
+		return (-1);
+	}
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			result = format_handler(format[i + 1], args);
 
-            if (result == NULL)
-            {
-                len += _putchar(format[i]);
-                len += _putchar(format[i + 1]);
-            }
-            else
-                len += result;
-            i += 2;
-        }
-        else
-        {
-            len += _putchar(format[i]);
-            i++;
-        }
-    }
+			if (result == -1)
+			{
+				len += _putchar(format[i]);
+				len += _putchar(format[i + 1]);
+			}
+			else
+				len += result;
+			i += 2;
+		}
+		else
+		{
+			len += _putchar(format[i]);
+			i++;
+		}
+	}
 
-    va_end(args);
+	va_end(args);
 
-    return (len);
+	return (len);
 }
